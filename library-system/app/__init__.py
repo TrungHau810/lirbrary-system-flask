7c9c2fc49ddc2a62e7dfd805d35615b4d04bc6af
ThuanProfessor <beanheo2014@gmail.com>
2025-09-20 19:43:43 +0700

    setting configs login

--- library-system/app/__init__.py
+++ library-system/app/__init__.py
@@ -5,9 +5,14 @@ from flask import Flask
 
 from flask_migrate import Migrate
 from flask_login import LoginManager
-
+from .models import User
+from .auth.routes import au
 app = Flask(__name__)
 
+
+db = SQLAlchemy()
+login = LoginManager()
+login.login_view = 'auth.login'
 
 app.secret_key = 'HGHJAHA^&^&*AJAVAHJ*^&^&*%&*^GAFGFAG'
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@localhost/library_system_db?charset=utf8mb4" % quote("Admin@123")
@@ -17,4 +22,12 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 from . import models
 from . import index
+
+login.init_app(app)
+
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+
 # login = LoginManager(app)
